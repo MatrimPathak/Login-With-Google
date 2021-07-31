707aefd7b1db8008d379ea49f6e9a2ca8f631581
MatrimPathak <63013617+MatrimPathak@users.noreply.github.com>
2021-07-31 12:48:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const uname = document.querySelector(".username .uname"),
+ppp = document.querySelector(".profile .ppp"),
+email = document.querySelector(".emailid .email"),
+gid = document.querySelector(".googleid .gid");
+
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  gid.textContent = profile.getId();
+  uname.textContent = profile.getName();
+  ppp.textContent = profile.getImageUrl();
+  email.textContent = profile.getEmail();
+}
