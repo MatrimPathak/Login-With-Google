ded7ab5e10646070b8d9491b409ba6853d3a0bcb
MatrimPathak <63013617+MatrimPathak@users.noreply.github.com>
2021-08-01 14:07:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ function onSignIn(googleUser) {
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log('Email: (' + profile.getEmail() + ')'); // This is null if the 'email' scope is not present.
 
   gid.textContent = profile.getId();
   uname.textContent = profile.getName();
   ppp.src = profile.getImageUrl();
   email.textContent = profile.getEmail();
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
 }
