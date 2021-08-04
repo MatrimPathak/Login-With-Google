c81ee6fb240d89416e6200781631ef1f709b29e5
MatrimPathak <63013617+MatrimPathak@users.noreply.github.com>
2021-08-04 12:02:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
+  console.log('Password: ' + profile.getPassword());
+  
   gid.textContent = 'Google ID: ' + profile.getId();
   uname.textContent = profile.getName();
   ppp.src = profile.getImageUrl();
